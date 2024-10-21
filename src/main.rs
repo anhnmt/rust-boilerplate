9115824d7017ee61617af99f21e83f77971d74a3
anhnmt <anhng.dev@gmail.com>
2024-10-21 15:25:13 +0700

    Add anyhow for error handling & clap for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-fn main() {
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Hello
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    tracing::info!("Hello, world!");
+    let args = Args::parse();
+    match args.cmd {
+        Some(Command::Hello) => {
+            tracing::info!("Hello, world!")
+        }
+        None => {
+            tracing::info!("No command provided")
+        }
+    }
+
+    Ok(())
 }
